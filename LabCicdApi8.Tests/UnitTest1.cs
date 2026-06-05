6e901f8971ce11b9f4241d35bb76600f6f39530e
Marcelo de Oliveira <marcelosdeoliveiras@gmail.com>
2026-06-04 21:30:03 -0300

    adiciona projeto de testes e solution

---
+++ LabCicdApi8.Tests/UnitTest1.cs
@@ -0,0 +1,32 @@
+using LabCicdApi8;
+
+namespace LabCicdApi8.Tests;
+
+public class WeatherForecastTests
+{
+    [Fact]
+    public void WeatherForecast_TemperatureF_DeveCalcularParaZeroGraus()
+    {
+        // Arrange
+        var forecast = new WeatherForecast(DateOnly.FromDateTime(DateTime.Now), 0, "Freezing");
+
+        // Act
+        var tempF = forecast.TemperatureF;
+
+        // Assert
+        Assert.Equal(32, tempF);
+    }
+
+    [Fact]
+    public void WeatherForecast_TemperatureF_DeveCalcularParaCemGraus()
+    {
+        // Arrange
+        var forecast = new WeatherForecast(DateOnly.FromDateTime(DateTime.Now), 100, "Hot");
+
+        // Act
+        var tempF = forecast.TemperatureF;
+
+        // Assert
+        Assert.Equal(211, tempF);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -28,7 +28,7 @@ app.MapGet("/weatherforecast", () =>
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
